f844ee627c768a3d4b7ea759dc7c8ac81652b73f
Gabriel Oliveira <gab14.oliveira2008@gmail.com>
2026-05-25 21:44:24 -0300

    Add files via upload

---
+++ alertar_estoque_baixo.py
@@ -0,0 +1,14 @@
+from cadastro_de_produtos import estoque
+def alertar_estoque_baixo(limite):
+    print("\n=== -Estoque Baixo- ===")
+
+    encontrou = False
+
+    for produto, dados in estoque.items():
+
+        if dados["quantidade"] <= limite:
+            print(f"{produto} -> Quantidade: {dados['quantidade']}")
+            encontrou = True
+
+    if not encontrou:
+        print("Nenhum produto encontrado com estoque baixo. ")
