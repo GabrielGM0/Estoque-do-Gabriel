6ea1566d68e0c4e2ebce54fe5cc07e26c60d539f
Gabriel Oliveira <gab14.oliveira2008@gmail.com>
2026-05-27 11:08:35 -0300

    Update consultar_estoque.py

--- consultar_estoque.py
+++ consultar_estoque.py
@@ -3,9 +3,11 @@ from cadastro_de_produtos import estoque
 def consultar_estoque(produto_id):
 
     for produto in estoque:
-        if produto['item'].lower() == produto_id.lower():
-            print(f"Produto: {produto['item']}")
-            print(f"Quantidade em estoque: {produto['quantidade']}")
-            return produto['quantidade']
+        
+        if produto.lower() == produto_id.lower():
+            print(f"Produto: {produto}")
+            print(f"Quantidade em estoque: {estoque[produto]['quantidade']}")
+            return estoque[produto]['quantidade']
+            
     print(f"Produto '{produto_id}' não encontrado.")
     return None
